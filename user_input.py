a9aaf9b2b048f530aaa1cf08569184872adef0b1
ALFRED KOMBO EMALI <160015941+emali-alfred@users.noreply.github.com>
2024-02-28 21:03:57 +0300

    Add files via upload
    This is the week one "Intro To Python Assignment."

---
+++ user_input.py
@@ -0,0 +1,10 @@
+# user_input.py
+
+name = ("ALFRED KOMBO EMALI ")
+age = (23)
+location = ("upper kabete campus uon")
+
+print(name)
+print(age)
+print(location)
+print("Hello [ALFRED KOMBO EMALI], I am [23] years old and live in [Upper Kabete Campus UON].")
